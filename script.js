43e81356e5f9ab44e01fac8c8330c4dc73d7a013
Servomouse <servomouse@protonmail.com>
2024-09-20 20:02:40 -0600

    Add open file functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let jsonFileContent = '';
+let jsonFileContent = null;
 function buttonClicked() {
 	alert('Button clicked!');
 }
@@ -115,8 +115,13 @@ function openCard() {
     // const overlay = document.getElementById('overlay');
     // card.classList.remove("hidden");
     // overlay.classList.add("visible");
-    document.getElementById('quiz-card').classList.remove("hidden");
-	document.getElementById('overlay').classList.add('visible');
+    if(jsonFileContent) {
+        document.getElementById('quiz-card').classList.remove("hidden");
+        document.getElementById('overlay').classList.add('visible');
+        fillQuizCard('Hola!', "Hola Pipiska!", ["привет", "тутуту", "колбаска", "ура"]);
+    } else {
+        console.log('Open file to start quiz');
+    }
 }
 
 function closeCard() {
@@ -125,8 +130,34 @@ function closeCard() {
 }
 closeCard();
 
-function fillQuizCard() {
-    const card = document.getElementById('quiz-card');
+function shuffleArray(array) {
+    for (let i=array.length-1; i>0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// answer[0] should be correct
+function fillQuizCard(word, example, answers) {
+    const answer0 = answers[0];
+    const shuffledArray = shuffleArray(answers);
+    document.getElementById('quiz-word').innerHTML = word;
+    document.getElementById('quiz-example').innerHTML = `Example: ${example}`;
+    console.log(shuffledArray, correctAnswer);
+    for(let i=0; i<4; i++) {
+        const answer = document.getElementById(`quiz-answer${i}`);
+        answer.innerHTML = shuffledArray[i];
+        if(shuffledArray[i] == answer0) {
+            answer.onclick = function() {correctAnswer();};
+        } else {
+            answer.onclick = function() {incorrectAnswer();};
+        }
+    }
+    // document.getElementById('quiz-answer0').innerHTML = shuffledArray[0];
+    // document.getElementById('quiz-answer1').innerHTML = shuffledArray[1];
+    // document.getElementById('quiz-answer2').innerHTML = shuffledArray[2];
+    // document.getElementById('quiz-answer3').innerHTML = shuffledArray[3];
 }
 
 function openFile() {
